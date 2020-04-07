f56a97dc17428cd9e4dfdc9fa8131df5bbc4efe8
Jochem van der Valk <jochemvandervalk@gmail.com>
2020-04-07 22:03:19 +0200

    Add files via upload

---
+++ lasertag/precache-manifest.e498f5b2aa86ac7c8367db41da379dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e100b5354376a212c5fa2292a55212",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71735d02dc4f74da1a52",
+    "url": "/static/css/main.ce3d27c5.chunk.css"
+  },
+  {
+    "revision": "d22d31ac229ea25f8385",
+    "url": "/static/js/2.b12cad2e.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/static/js/2.b12cad2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71735d02dc4f74da1a52",
+    "url": "/static/js/main.883a18f5.chunk.js"
+  },
+  {
+    "revision": "26a6d367018f5ed0a766",
+    "url": "/static/js/runtime-main.59dc6da7.js"
+  }
+]);
