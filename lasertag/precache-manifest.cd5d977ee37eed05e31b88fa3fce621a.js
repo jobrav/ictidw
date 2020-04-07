a0ac7df7395ff55cfed26ab76c1deee77f777be3
Jochem van der Valk <jochemvandervalk@gmail.com>
2020-04-07 22:14:40 +0200

    Add files via upload

---
+++ lasertag/precache-manifest.cd5d977ee37eed05e31b88fa3fce621a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0159e4e8d745e3fe466c5903af79cd8",
+    "url": "/ictidw/lasertag/index.html"
+  },
+  {
+    "revision": "1ab93c6d54c22b52d2c0",
+    "url": "/ictidw/lasertag/static/css/main.ce3d27c5.chunk.css"
+  },
+  {
+    "revision": "921d67cb8598a70279c1",
+    "url": "/ictidw/lasertag/static/js/2.7ad81183.chunk.js"
+  },
+  {
+    "revision": "4c86fd10a56eb60a384cd2f2c8426c35",
+    "url": "/ictidw/lasertag/static/js/2.7ad81183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ab93c6d54c22b52d2c0",
+    "url": "/ictidw/lasertag/static/js/main.41546458.chunk.js"
+  },
+  {
+    "revision": "a5d43c1d25ebba1ec549",
+    "url": "/ictidw/lasertag/static/js/runtime-main.2f92514b.js"
+  }
+]);
--- lasertag/service-worker.js
+++ lasertag/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e498f5b2aa86ac7c8367db41da379dfd.js"
+  "/ictidw/lasertag/precache-manifest.cd5d977ee37eed05e31b88fa3fce621a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ictidw/lasertag/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
